b749ccac3a6e6607376120bb69024a8a06cb483c
adamwinzdesign <adamwinzdesign@gmail.com>
2020-05-04 23:15:25 -0500

    added to dictionaries

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,6 +35,9 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append({"lat": 41, "lon": -134, "name": "custom location"})
+
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
@@ -42,6 +45,17 @@ waypoints = [
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[0]['lon'] = -130
+waypoints[0]['name'] = 'not a real place'
+
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+def get_values(dict):
+    for x in dict:
+        print(list(x.values()))
+
+print(get_values(waypoints))
+
+# why do we get None at the end of the result here?
